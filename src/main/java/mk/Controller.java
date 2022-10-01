8fca8b465647732be9b81c3826567afb20688467
MichaelKorostashevskii <michaelkorostashevskii@gmail.com>
2022-10-01 14:35:36 +0300

    test

--- src/main/java/mk/Controller.java
+++ src/main/java/mk/Controller.java
@@ -4,5 +4,6 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("text");
         System.out.println("do");
+        System.out.println("a");
     }
 }
